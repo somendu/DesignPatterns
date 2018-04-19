661bb2b8c836f9efa322aaa3c4d22a5cf752fb8b
somendu <somendu.maiti@gmail.com>
2018-04-19 23:13:52 +0530

    Test New

---
+++ src/main/java/com/somendu/design/singleton/TestNew.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.somendu.design.singleton;
+
+/**
+ * @author Somendu
+ *
+ */
+public class TestNew {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
