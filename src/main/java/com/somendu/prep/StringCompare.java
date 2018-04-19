863e39acc49e1dc3cee35033e7722589f7e1c536
somendu <somendu.maiti@gmail.com>
2018-04-19 21:39:40 +0530

    test

---
+++ src/main/java/com/somendu/prep/StringCompare.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.somendu.prep;
+
+/**
+ * @author Somendu
+ *
+ */
+public class StringCompare {
+
+	/**
+	 * @param args
+	 */
+
+	private int x = 99, y = 100;
+
+	public StringCompare(double y) {
+		System.out.println(y);
+	}
+
+	public String toString() {
+		return "[" + x + " , " + y + "]";
+	}
+
+	public static void main(String[] args) {
+		int x = 99, y = 100;
+
+		boolean b = true;
+		System.out.println(x);
+		System.out.println(25.0 / 4.0);
+		System.out.println(25.0 / 4);
+	}
+
+}
