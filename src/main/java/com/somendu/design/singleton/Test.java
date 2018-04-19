79fb83947e216159a9a19a49d929d617685d7399
somendu <somendu.maiti@gmail.com>
2018-04-19 23:17:19 +0530

    TestCommit

---
+++ src/main/java/com/somendu/design/singleton/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.somendu.design.singleton;
+
+/**
+ * @author Somendu
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
