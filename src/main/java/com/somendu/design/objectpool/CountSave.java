93d607e2bfbdde41f8fd52944ff522f53d3d27e6
somendu <somendu.maiti@gmail.com>
2018-04-19 20:50:07 +0530

    Test File

---
+++ src/main/java/com/somendu/design/objectpool/CountSave.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.somendu.design.objectpool;
+
+import java.io.File;
+
+/**
+ * @author Somendu
+ *
+ */
+public class CountSave {
+
+	File file = new File("count.txt");
+
+	int countStore = 0;
+
+	String writeFile() {
+
+		return "";
+	}
+
+}
